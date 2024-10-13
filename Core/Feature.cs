a047e859f250881b53e5fbee5ec2a8c5d0cfb5f7
Vinicius Rocha <viniciusroch65@gmail.com>
2024-10-13 16:36:54 -0300

    init: initialize solution

---
+++ Core/Feature.cs
@@ -0,0 +1,15 @@
+﻿namespace Core;
+
+public record class Feature<TValue>(int Index, List<TValue> Labels, double InformationGain) where TValue : IComparable<TValue>
+{
+  public static List<TValue> GetFeatureValuesFromDataset(List<List<TValue>> data, int featureIndex) =>
+    data.Select(row => row[featureIndex]).Distinct().ToList();
+
+  public double Entropy =>
+    CalculateClassProbabilities().Sum(probability => -probability * Math.Log(probability, 2));
+
+  public List<double> CalculateClassProbabilities() =>
+    Labels.Select(label => (double)Labels.Count(otherLabel => otherLabel.Equals(label)) / Labels.Count).ToList();
+
+  public double Importance<TDataValue>(List<TDataValue> data) => data.Count - InformationGain;
+}
