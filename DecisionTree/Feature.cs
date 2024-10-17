6a93750707b3fdd8782ee56b7218eaf0e60ccf96
Vinicius Rocha <viniciusroch65@gmail.com>
2024-10-17 13:33:57 -0300

    feat: add feature record class

---
+++ DecisionTree/Feature.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+
+namespace DecisionTree;
+
+/// <summary>
+/// Represents a feature in a decision tree.
+/// </summary>
+/// <typeparam name="TValue">The type of the values for the feature.</typeparam>
+/// <param name="Name">The name of the feature.</param>
+/// <param name="Values">The list of possible values for the feature.</param>
+public record class Feature<TValue>(string Name, List<TValue> Values)
+{
+  public static List<TValue> GetDifferentValues(DataTable data, int featureIndex) => data.AsEnumerable().Select(row => (TValue)row[featureIndex]).Distinct().ToList();
+}
