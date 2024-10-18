4db2db1bd19043e8f3ecc36e7dbbe81ce2288a6c
Vinicius Rocha <viniciusroch65@gmail.com>
2024-10-18 09:30:46 -0300

    feat: add node's children

--- DecisionTree/Node.cs
+++ DecisionTree/Node.cs
@@ -4,6 +4,7 @@ public record class Node<TEdgeValue, TFeatureValue>(
     string Name,
     bool IsLeaf,
     TEdgeValue Edge,
+    List<Node<TEdgeValue, TFeatureValue>> Children,
     Feature<TFeatureValue>? Feature
 )
 {
