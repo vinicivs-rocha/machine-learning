fe0186eafc60120b2a18584d181ad9c48ad4bab8
Vinicius Rocha <viniciusroch65@gmail.com>
2024-10-18 09:24:55 -0300

    refactor: format files

--- DecisionTree/Feature.cs
+++ DecisionTree/Feature.cs
@@ -8,7 +8,8 @@ namespace DecisionTree;
 /// <typeparam name="TValue">The type of the values for the feature.</typeparam>
 /// <param name="Name">The name of the feature.</param>
 /// <param name="Values">The list of possible values for the feature.</param>
-public record class Feature<TValue>(string Name, List<TValue> Values)
+public abstract record class Feature<TValue>(string Name, List<TValue> Values)
 {
-  public static List<TValue> GetDifferentValues(DataTable data, int featureIndex) => data.AsEnumerable().Select(row => (TValue)row[featureIndex]).Distinct().ToList();
+    public static List<TValue> GetDifferentValues(DataTable data, int featureIndex) =>
+        data.AsEnumerable().Select(row => (TValue)row[featureIndex]).Distinct().ToList();
 }
--- DecisionTree/Node.cs
+++ DecisionTree/Node.cs
@@ -1,9 +1,10 @@
 ﻿namespace DecisionTree;
 
 public record class Node<TEdgeValue, TFeatureValue>(
-  string Name,
-  bool IsLeaf,
-  TEdgeValue Edge,
-  Feature<TFeatureValue>? Feature
+    string Name,
+    bool IsLeaf,
+    TEdgeValue Edge,
+    Feature<TFeatureValue>? Feature
 )
-{}
+{
+}
