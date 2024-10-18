09a6bb65d43bbaba7470b128d52f8ba77ca3d34e
Vinicius Rocha <viniciusroch65@gmail.com>
2024-10-18 13:47:38 -0300

    feat: add get best node method to ID3 class

--- DecisionTree/Id3.cs
+++ DecisionTree/Id3.cs
@@ -2,27 +2,24 @@ using System.Data;
 
 namespace DecisionTree;
 
-public class Id3<TNodeEdgeValue, TNodeFeatureValue>(DataTable trainingData) where TNodeFeatureValue : notnull
+public class Id3<TNodeEdgeValue, TNodeFeatureValue>(DataTable trainingData)
+    where TNodeFeatureValue : notnull
+    where TNodeEdgeValue : notnull
 {
     public readonly Node<TNodeEdgeValue, TNodeFeatureValue> Root =
-        Id3<TNodeEdgeValue, TNodeFeatureValue>.GetRootNode(trainingData);
+        Id3<TNodeEdgeValue, TNodeFeatureValue>.GetBestNode(trainingData, trainingData.Columns.Count - 1);
 
-    private static Node<TNodeEdgeValue, TNodeFeatureValue> GetRootNode(DataTable trainingData)
+    private static Node<TNodeEdgeValue, TNodeFeatureValue> GetBestNode(DataTable parentData, int parentColumnIndex,
+        TNodeEdgeValue edgeValue = default!)
     {
-        var rootNodeColumnIndex = int.MaxValue;
-        var highestInformationGain = double.MinValue;
-        var trainingDataEntropy = Id3<TNodeEdgeValue, TNodeFeatureValue>.CalculateTrainingDataEntropy(trainingData);
+        var features = parentData.Columns.Cast<DataColumn>().Select(column =>
+            new Feature<TNodeFeatureValue>(column.ColumnName,
+                Feature<TNodeFeatureValue>.CalculateInformationGain(parentData, column.Ordinal),
+                Feature<TNodeFeatureValue>.GetDifferentValues(parentData, column.Ordinal)));
+        var bestFeature = features.Aggregate((best, current) =>
+            current.InformationGain > best.InformationGain ? current : best);
+
+        return new Node<TNodeEdgeValue, TNodeFeatureValue>(IsLeaf: false, Edge: edgeValue, Feature: bestFeature,
+            Children: []);
     }
-
-    private static double CalculateTrainingDataEntropy(DataTable trainingData) =>
-        Id3<TNodeEdgeValue, TNodeFeatureValue>.CalculateFeatureEntropy(trainingData, trainingData.Columns.Count - 1);
-
-    private static double CalculateFeatureEntropy(DataTable trainingData, int featureIndex) =>
-        (from DataRow row in trainingData.Rows
-            let value = (TNodeFeatureValue)row[featureIndex]
-            group value by value
-            into groupedValues
-            let labelCount = groupedValues.Count()
-            let probability = (double)labelCount / trainingData.Rows.Count
-            select -probability * Math.Log(probability, 2)).Sum();
 }
